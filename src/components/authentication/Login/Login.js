d6635d8b4e7a8076b4259b970c4e66f3b650cb96
xerxes23 <josephmendoza23@gmail.com>
2020-12-26 13:20:19 -0500

    Started file structure

---
+++ src/components/authentication/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+	return (
+		<div>
+			<div>email</div>
+			<div>password</div>
+			<button>login</button>
+		</div>
+	);
+}
---
+++ src/components/authentication/signup/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const SignUp = () => {
+	return (
+		<div>
+			<div>name</div>
+			<div>birthday</div>
+			<div>email</div>
+			<div>password</div>
+		</div>
+	);
+};
