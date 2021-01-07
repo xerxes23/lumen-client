d489a62d0edf62487355790a0aa5ca621f1980d2
xerxes23 <josephmendoza23@gmail.com>
2021-01-06 23:34:54 -0500

    Added button to navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
 	return (
 		<div>
 			<h1>Lumenboard</h1>
-			<div>Add Question</div>
+			<button>Add Question</button>
 			<div>Profile</div>
 			<div>Logout</div>
 		</div>
