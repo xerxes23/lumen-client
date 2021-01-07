5429a57d6f84d0ea43d0728e49402116f20b8ff3
xerxes23 <josephmendoza23@gmail.com>
2021-01-07 16:06:56 -0500

    Styled Navbar component

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+	navbar: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		padding: 20,
+	},
+	logo: {
+		display: "flex",
+		fontSize: 32,
+		fontWeight: "bolder",
+	},
+	navbarLinks: {
+		display: "flex",
+		flexDirection: "row",
+	},
+});
 
 export function Navbar() {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<h1>Lumenboard</h1>
-			<button>Add Question</button>
-			<div>Profile</div>
-			<div>Logout</div>
+		<div className={classes.navbar}>
+			<div className={classes.logo}>Lumenboard</div>
+			<div className={classes.navbarLinks}>
+				<div>Add Question</div>
+				<div>Profile</div>
+				<div>Logout</div>
+			</div>
 		</div>
 	);
 }
